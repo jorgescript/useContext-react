0a1416ed1cb31aa79561b2a8aa57516a187fae56
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-13 22:35:24 -0500

    uso del NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
   return (
@@ -6,6 +6,12 @@ export const NavBar = () => {
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/login">Login</Link>
+      <NavLink
+        to="/login"
+        className={(args) => `${args.isActive ? "active" : ""}`}
+      >
+        Login
+      </NavLink>
     </>
   );
 };
